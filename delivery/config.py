e28617389834e87fa19ea7bdc3d1d61f78975612
Konstantin Ponomarev <konstantinponomarev@protonmail.com>
2020-10-02 02:42:11 +0300

    added config

---
+++ delivery/config.py
@@ -0,0 +1,12 @@
+import secrets
+import os
+
+class Config:
+    SECRET_KEY = secrets.token_urlsafe(32)
+    MONGODB_DB = 'delivery'
+    FLASK_DEBUG = False
+    FIRST_RUN = False
+    CAPTCHA_SERVER_KEY = os.environ['CAPTCHA_SERVER_KEY'] # google recaptcha
+    CAPTCHA_CLIENT_KEY = os.environ['CAPTCHA_CLIENT_KEY']
+    PASSWORD = 'superVeryLongAdminPassword'
+
