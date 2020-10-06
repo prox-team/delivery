03b80adcfb96540ac44b41f83343257880560439
Konstantin Ponomarev <ponomarevkonst@gmail.com>
2020-10-06 22:27:37 +0000

    added nginx

--- delivery/config.py
+++ delivery/config.py
@@ -3,10 +3,11 @@ import os
 
 class Config:
     SECRET_KEY = secrets.token_urlsafe(32)
+    MONGODB_HOST = 'mongodb'
     MONGODB_DB = 'delivery'
     FLASK_DEBUG = False
     FIRST_RUN = False
-    CAPTCHA_SERVER_KEY = os.environ['CAPTCHA_SERVER_KEY'] # google recaptcha
-    CAPTCHA_CLIENT_KEY = os.environ['CAPTCHA_CLIENT_KEY'] # google recaptcha
+    CAPTCHA_SERVER_KEY = '6LctT7wZAAAAABHdfuFF2PMUljFyZfC3Ai6_FKND'  # google recaptcha
+    CAPTCHA_CLIENT_KEY = '6LctT7wZAAAAAEuScMNpqsw9zXnNUMLvY_lq9_Xr' # google recaptcha
     PASSWORD = 'superVeryLongAdminPassword'
 
